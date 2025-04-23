6ee159ea9788a597fbc9ac352d65efa47f975f43
Craig Smith <craig@littlejourney.health>
2025-04-23 19:51:55 +0100

    Added new theme and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,66 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useInstallPrompt } from './useInstallPrompt';
-import { useSpeechRecognition } from './useSpeechRecognition';
-import { stringSimilarity } from './utils';
+import { useState } from 'react';
+import divider from './assets/divider.svg';
 import './App.css';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const prompt = useInstallPrompt();
-  const recognition = useSpeechRecognition();
-  const ref = useRef<HTMLDivElement>(null);
+  const [screen, setScreen] = useState(0);
 
-  const onInstall = async () => {
-    prompt?.prompt();
+  const handleNext = () => {
+    setScreen((screen + 1) % 4);
   };
-
-  const onListen = useCallback(() => {
-    recognition.start();
-  }, [recognition]);
-
-  const onRegcognitionResult = useCallback((event: SpeechRecognitionEvent) => {
-    const result = event.results[0];
-    const text = result[0].transcript;
-
-    console.log(text);
-
-    if (ref.current) {
-      ref.current.textContent = text;
-    }
-
-    if (result.isFinal) {
-      console.log('match: ', stringSimilarity(text, 'The hall was filled with portraits and statues, watching silently.'));
-    }
-  }, []);
-
-  useEffect(() => {
-    recognition.addEventListener('result', onRegcognitionResult);
-    return () => {
-      recognition.removeEventListener('result', onRegcognitionResult);
-    }
-  }, [onRegcognitionResult, recognition]);
-
+  
   return (
-    <div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={onInstall}>
-          install
-        </button>
-        <a href="/treasure-hunt/" target="_blank">
-          open
-        </a>
-        <button onClick={onListen}>
-          listen
-        </button>
+    <div className="view" data-screen={screen}>
+      <div className="background">
       </div>
-      <div className="output" ref={ref} />
+      <div className="duo1">
+      </div>
+      <div className="duo2">
+      </div>
+      <div className="screens">
+
+        {/* Welcome screen */}
+        <div className="screen">
+          <h1>Oulton Hall Treasure Hunt</h1>
+          <img className="divider" src={divider} />
+          <div className="grow" />
+          <div className="actions">
+            <button onMouseDown={handleNext}>Start</button>
+          </div>
+        </div>
+
+        {/* Instructions */}
+        <div className="screen">
+          <h2>The hunt is on!</h2>
+          <img className="divider" src={divider} />
+          <div className="grow" />
+          <div className="actions">
+            <button onMouseDown={handleNext}>Next</button>
+          </div>
+        </div>
+
+        {/* Enter team name */}
+        <div className="screen">
+          <h2>Choose your team name</h2>
+          <img className="divider" src={divider} />
+          <div className="grow" />
+          <div className="actions">
+            <button onMouseDown={handleNext}>Next</button>
+          </div>
+        </div>
+
+        {/* Home screen */}
+        <div className="screen">
+          <h1>0/7</h1>
+          <h1>Books<br />found</h1>
+          <img className="divider" src={divider} />
+          <div className="grow" />
+          <div className="actions">
+            <button onMouseDown={handleNext}>Next</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 };
---
+++ src/App2.tsx
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useInstallPrompt } from './useInstallPrompt';
+import { useSpeechRecognition } from './useSpeechRecognition';
+import { stringSimilarity } from './utils';
+import './App.css';
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+  const prompt = useInstallPrompt();
+  const recognition = useSpeechRecognition();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onInstall = async () => {
+    prompt?.prompt();
+  };
+
+  const onListen = useCallback(() => {
+    recognition.start();
+  }, [recognition]);
+
+  const onRegcognitionResult = useCallback((event: SpeechRecognitionEvent) => {
+    const result = event.results[0];
+    const text = result[0].transcript;
+
+    console.log(text);
+
+    if (ref.current) {
+      ref.current.textContent = text;
+    }
+
+    if (result.isFinal) {
+      console.log('match: ', stringSimilarity(text, 'The hall was filled with portraits and statues, watching silently.'));
+    }
+  }, []);
+
+  useEffect(() => {
+    recognition.addEventListener('result', onRegcognitionResult);
+    return () => {
+      recognition.removeEventListener('result', onRegcognitionResult);
+    }
+  }, [onRegcognitionResult, recognition]);
+
+  return (
+    <div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={onInstall}>
+          install
+        </button>
+        <a href="/treasure-hunt/" target="_blank">
+          open
+        </a>
+        <button onClick={onListen}>
+          listen
+        </button>
+      </div>
+      <div className="output" ref={ref} />
+    </div>
+  )
+};
